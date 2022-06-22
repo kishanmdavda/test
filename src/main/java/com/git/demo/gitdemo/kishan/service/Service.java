3d9fc016b126ec2bffe0ab292b9b8bee1271db17
kishanm <jayden_reid@divinedigitalagency.com>
2022-06-22 19:07:29 +0530

    Added service class in the project and created testService() method.

---
+++ src/main/java/com/git/demo/gitdemo/kishan/service/Service.java
@@ -0,0 +1,10 @@
+package com.git.demo.gitdemo.kishan.service;
+
+@org.springframework.stereotype.Service
+public class Service {
+    public String testService()
+    {
+        System.out.println("Service");
+        return "Service";
+    }
+}
